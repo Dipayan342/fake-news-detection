29ccdcb7998d9399b55d9f65a744a495b84c14d6
Dipayan342 <dipayanguha183@gmail.com>
2025-04-03 21:15:41 +0530

    Add files via upload
    commited initial changes

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import string
+import re
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score 
+from sklearn.metrics import classification_report
