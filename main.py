271fbf4c5118f50198f9a0552f822e1e445fc3c0
Dipayan342 <dipayanguha183@gmail.com>
2025-04-03 23:14:21 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-import seaborn as sns
-import string
-import re
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score 
-from sklearn.metrics import classification_report
