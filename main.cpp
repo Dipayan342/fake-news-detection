7bf538d75cbe0be8c49455d33370af71d6b319b6
Dipayan342 <dipayanguha183@gmail.com>
2025-04-04 15:02:28 +0530

    Add files via upload
    commit6

---
+++ main.cpp
@@ -0,0 +1,10 @@
+project/
+│
+├── fake_news_detection.ipynb
+├── model.pkl                
+├── vectorizer.pkl           
+├── main.py                  
+├── templates/
+│   └── index.html
+├── static/
+│   └── style.css
