78d9055f9c38a213d7cef56a332c4adf0d9016d4
Dipayan342 <dipayanguha183@gmail.com>
2025-06-15 15:26:11 +0530

    Delete main.cpp

--- main.cpp
+++
@@ -1,8 +0,0 @@
-project/
-│
-├── main.py                ← FastAPI backend
-├── model.pkl              ← Your ML model
-├── vectorizer.pkl         ← Your vectorizer
-├── index.html             ← Your frontend
-├── style.css              ← CSS file
-
